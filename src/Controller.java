9c1356845698f7c6c891946d032e79c3e02f84a0
ek223gi <ek223gi@student.lnu.se>
2020-11-03 00:06:41 +0300

    Middle and Controller classes are added

---
+++ src/Controller.java
@@ -0,0 +1,9 @@
+package src;
+
+public class Controller {
+
+    public String Win(Player player1, Player player2){
+
+        return "";
+    }
+}
---
+++ src/Middle.java
@@ -0,0 +1,22 @@
+package src;
+
+import java.util.ArrayList;
+
+public class Middle {
+
+    private String card1;
+    private String card2;
+    private String card3;
+    private String card4;
+    private String card5;
+
+    public Middle(ArrayList<String> arrm) {
+        card1 = arrm.get(0);
+        card2 = arrm.get(1);
+        card3 = arrm.get(2);
+        card4 = arrm.get(3);
+        card5 = arrm.get(4);
+
+    }
+}
+
